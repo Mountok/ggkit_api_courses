92a177da866173f716cc68c2c14d6c919793abaf
admin <admin@MacBook-Pro13-Mountok.local>
2024-09-30 17:55:50 +0300

    edit config.go

--- internals/cfg/config.go
+++ internals/cfg/config.go
@@ -3,6 +3,7 @@ package cfg
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/go-redis/redis/v8"
 	log "github.com/sirupsen/logrus"
@@ -21,14 +22,13 @@ type Cfg struct {
 func LoadConfig() Cfg {
 	v := viper.New()
 	v.SetEnvPrefix("GGKIT_SERV")
-	v.Set("PORT", "8080")
-
+	// v.Set("PORT", "8080")
 	// v.Set("DBNAME", "postgres")
 	// v.Set("DBUSER", "postgres")
 	// v.Set("DBPASS", "admin")
 	// v.Set("DBHOST", "127.0.0.1")
 	// v.Set("DBPORT", "5436")
-	// v.Set("PORT", os.Getenv("PORT"))
+	v.Set("PORT", os.Getenv("PORT"))
 	v.Set("DBNAME", "railway")
 	v.Set("DBUSER", "postgres")
 	v.Set("DBPASS", "JMBBpmeyasyiQWhdpLxjESwTwsocyehv")
